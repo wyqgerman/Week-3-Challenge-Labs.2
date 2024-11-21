ae5f8cdbf15d670cd81da74c406b73964a543601
Ethan Ying Wang <Yingqiu.Wang@mssa.newhorizons.com>
2024-11-21 16:55:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Week_3_Challenge_Labs._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(SumDigitsInString("1q2w3e"));       
+            Console.WriteLine(SumDigitsInString("L0r3m.1p5um"));  
+            Console.WriteLine(SumDigitsInString(""));             
+        }
+
+        static int SumDigitsInString(string input)
+        {
+            int sum = 0;
+
+            foreach (char ch in input)
+            {
+                if (char.IsDigit(ch))
+                {
+                    sum += ch - '0'; 
+                }
+            }
+
+            return sum;
+        }
+    }
+}
